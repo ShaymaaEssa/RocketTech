2daf5b108c7446e7d6acc23ec0937fcf36cc3b52
ShaymaaEssa <flowrathesky@gmail.com>
2025-03-26 23:32:44 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist' // Ensures output goes to the correct folder
-  }
+  },
+  publicDir: 'public' // Ensures fonts and images are included
 });
